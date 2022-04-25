fffa8fe5176021664a480d2707e27d614878166a
Abdulmughni <Abuzainah.abdulmughni@gmail.com>
2022-04-25 06:03:43 +0300

    Setting up project files, building models to represent database tables and fixing names of the table columns to follow camel case convention.

---
+++ src/main/java/com/udacity/cloudstorage/Models/Credential.java
@@ -0,0 +1,10 @@
+package com.udacity.cloudstorage.Models;
+
+public class Credential {
+    private Integer credentialId;
+    private String url;
+    private String username;
+    private String key;
+    private String password;
+    private Integer userId;
+}
